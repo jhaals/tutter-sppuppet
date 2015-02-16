823748bca4cd52df376b3dd4199cd35cbd649ba5
Alexey Lapitsky <alexey@spotify.com>
2015-02-16 14:13:12 +0100

    configurable reports generation

--- lib/tutter/action/sppuppet.rb
+++ lib/tutter/action/sppuppet.rb
@@ -6,6 +6,7 @@ class Sppuppet
   def initialize(settings, client, project, data, event)
     @settings = settings
     @settings['plus_ones_required'] ||= 1
+    @settings['reports_dir'] ||= '/var/lib/tutter/reports'
     @client = client
     @project = project
     @data = data
@@ -93,10 +94,12 @@ class Sppuppet
                tests: @client.combined_status(@project, pr.head.sha).statuses.map { |s| {state: s.state, url: s.target_url, description: s.description } },
                reviewers: plus_one.keys,
                deployer: comments.last.user.login }
-      report_directory = "/var/lib/tutter/reports/#{merge_commit.sha[0..1]}/#{merge_commit.sha[2..3]}"
+      report_directory = "#{@settings['reports_dir']}/#{merge_commit.sha[0..1]}/#{merge_commit.sha[2..3]}"
       report_path = "#{report_directory}/#{merge_commit.sha}.json"
-      FileUtils.mkdir_p report_directory
-      File.open(report_path, 'w') { |f| f.write(JSON.pretty_generate(json)) }
+      if @settings['generate_reports']
+        FileUtils.mkdir_p report_directory
+        File.open(report_path, 'w') { |f| f.write(JSON.pretty_generate(json)) }
+      end
       return 200, "merging #{pull_request_id} #{@project}"
     elsif plus_one.count >= @settings['plus_ones_required']
       return 200, "have enough +1, but no merge command"
