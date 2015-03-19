2bd536a8f993fb58aa6b4f39cd4e0961c67505a1
Erik Dalén <dalen@spotify.com>
2015-03-19 15:06:07 +0100

    Refactor based on event type
    This makes it gracefully handle unknown event types. And the code for
    handling new PRs isn't mixed with the code for handling new comments.

--- lib/tutter/action/sppuppet.rb
+++ lib/tutter/action/sppuppet.rb
@@ -14,84 +14,65 @@ class Sppuppet
   end
 
   def run
-    # If a new pull request is opened, comment with instructions
-    if @data['action'] == 'opened' && @settings['post_instructions']
-      issue = @data['number']
-      comment = @settings['instructions'] || "To merge at least #{@settings['plus_ones_required']} person other than the submitter needs to write a comment with saying _+1_ or :+1:. Then write _!merge_ or :shipit: to trigger the merging."
-      begin
-        @client.add_comment(@project, issue, comment)
-        return 200, "Commented!"
-      rescue Octokit::NotFound
-        return 404, "Octokit returned 404, this could be an issue with your access token"
-      rescue Octokit::Unauthorized
-        return 401, "Authorization to #{@project} failed, please verify your access token"
-      rescue Octokit::TooManyLoginAttempts
-        return 429, "Account for #{@project} has been temporary locked down due to to many failed login attempts"
+    case @event
+    when 'issue_comment'
+      if @data['action'] != 'created'
+        # Not a new comment, ignore
+        return 200, 'not a new comment, skipping'
       end
-    end
 
-    if @data['action'] != 'created'
-      # Not a new comment, ignore
-      return 200, 'not a new comment, skipping'
-    end
+      pull_request_id = @data['issue']['number']
+      pr = @client.pull_request @project, pull_request_id
+      plus_one = {}
 
-    pull_request_id = @data['issue']['number']
-    pr = @client.pull_request @project, pull_request_id
-    plus_one = {}
-    merge = false
+      unless pr.mergeable
+        return 200, "merge state for #{@project} #{pull_request_id} is not clean. Current state: #{pr.mergeable_state}"
+      end
 
-    if pr.mergeable_state != 'clean'
-      return 200, "merge state for #{@project} #{pull_request_id} is not clean. Current state: #{pr.mergeable_state}"
-    end
+      # No comments, no need to go further.
+      return 200, 'no comments, skipping' if pr.comments == 0
 
-    # No comments, no need to go further.
-    if pr.comments == 0
-      return 200, 'no comments, skipping'
-    end
+      # We fetch the latest commit and it's date.
+      last_commit = @client.pull_request_commits(@project, pull_request_id).last
+      last_commit_date = last_commit.commit.committer.date
 
-    # Don't care about code we can't merge
-    return 200, 'merge state not clean' unless pr.mergeable
+      comments = @client.issue_comments(@project, pull_request_id)
 
-    # We fetch the latest commit and it's date.
-    last_commit = @client.pull_request_commits(@project, pull_request_id).last
-    last_commit_date = last_commit.commit.committer.date
+      # Check each comment for +1 and merge comments
+      comments.each do |i|
 
-    comments = @client.issue_comments(@project, pull_request_id)
+        # Comment is older than last commit. We only want to check for +1 in newer comments
+        next if last_commit_date > i.created_at
 
-    # Check each comment for +1 and merge comments
-    comments.each do |i|
+        if /^(\+1|:\+1)/.match i.body
+          # pull request submitter cant +1
+          unless pr.user.login == i.attrs[:user].attrs[:login]
+            plus_one[i.attrs[:user].attrs[:login]] = 1
+          end
+        end
 
-      # Comment is older than last commit. We only want to check for +1 in newer comments
-      next if last_commit_date > i.created_at
-
-      if /^(\+1|:\+1)/.match i.body
-        # pull request submitter cant +1
-        unless pr.user.login == i.attrs[:user].attrs[:login]
-          plus_one[i.attrs[:user].attrs[:login]] = 1
+        # TODO it should calculate the +1's - the -1's
+        # Never merge if someone says -1
+        if /^(\-1|:\-1:)/.match i.body
+          return 200, "#{@project} #{pull_request_id} has a -1. I will not take the blame"
         end
       end
 
-      # TODO it should calculate the +1's - the -1's
-      # Never merge if someone says -1
-      if /^(\-1|:\-1:)/.match i.body
-        return 200, "#{@project} #{pull_request_id} has a -1. I will not take the blame"
-      end
-    end
+      merge = (@data['comment']['body'] == '!merge' ||
+        @data['comment']['body'].start_with?(':shipit:'))
 
-    merge = (comments.last.body == '!merge' || comments.last.body.start_with?(':shipit:'))
-
-    if plus_one.count >= @settings['plus_ones_required'] && merge
-      json = { url: pr.url,
-               title: pr.title,
-               author: pr.user.login,
-               description: pr.body,
-               commits: @client.pull_request_commits(@project, pr.number).map { |c| { author: c.author, message: c.commit.message, sha: c.commit.tree.sha } },
-               head_sha: pr.head.sha,
-               tests: @client.combined_status(@project, pr.head.sha).statuses.map { |s| {state: s.state, url: s.target_url, description: s.description } },
-               reviewers: plus_one.keys,
-               deployer: comments.last.user.login }
-      # TODO: Word wrap description
-      merge_msg = <<MERGE_MSG
+      if plus_one.count >= @settings['plus_ones_required'] && merge
+        json = { url: pr.url,
+                 title: pr.title,
+                 author: pr.user.login,
+                 description: pr.body,
+                 commits: @client.pull_request_commits(@project, pr.number).map { |c| { author: c.author, message: c.commit.message, sha: c.commit.tree.sha } },
+                 head_sha: pr.head.sha,
+                 tests: @client.combined_status(@project, pr.head.sha).statuses.map { |s| {state: s.state, url: s.target_url, description: s.description } },
+                 reviewers: plus_one.keys,
+                 deployer: comments.last.user.login }
+        # TODO: Word wrap description
+        merge_msg = <<MERGE_MSG
 Title: #{pr.title}
 Description: #{pr.body}
 Author: #{pr.user.login}
@@ -99,19 +80,45 @@ Reviewers: #{plus_one.keys.join ', '}
 Deployer: #{comments.last.user.login}
 URL: #{pr.url}
 MERGE_MSG
-      merge_commit = @client.merge_pull_request(@project, pull_request_id, merge_msg)
-      json[:merge_sha] = merge_commit.sha
-      report_directory = "#{@settings['reports_dir']}/#{merge_commit.sha[0..1]}/#{merge_commit.sha[2..3]}"
-      report_path = "#{report_directory}/#{merge_commit.sha}.json"
-      if @settings['generate_reports']
-        FileUtils.mkdir_p report_directory
-        File.open(report_path, 'w') { |f| f.write(JSON.pretty_generate(json)) }
+        begin
+          merge_commit = @client.merge_pull_request(@project, pull_request_id, merge_msg)
+        rescue Octokit::MethodNotAllowed => e
+          return 200, "Pull request not mergeable: #{e.message}"
+        end
+        puts merge_commit.inspect
+        json[:merge_sha] = merge_commit.sha
+        report_directory = "#{@settings['reports_dir']}/#{merge_commit.sha[0..1]}/#{merge_commit.sha[2..3]}"
+        report_path = "#{report_directory}/#{merge_commit.sha}.json"
+        if @settings['generate_reports']
+          FileUtils.mkdir_p report_directory
+          File.open(report_path, 'w') { |f| f.write(JSON.pretty_generate(json)) }
+        end
+        return 200, "merging #{pull_request_id} #{@project}"
+      elsif plus_one.count >= @settings['plus_ones_required']
+        return 200, "have enough +1, but no merge command"
+      else
+        return 200, "not enough +1, have #{plus_one.count} but need #{@settings['plus_ones_required']}"
       end
-      return 200, "merging #{pull_request_id} #{@project}"
-    elsif plus_one.count >= @settings['plus_ones_required']
-      return 200, "have enough +1, but no merge command"
+    when 'pull_request'
+      # If a new pull request is opened, comment with instructions
+      if @data['action'] == 'opened' && @settings['post_instructions']
+        issue = @data['number']
+        comment = @settings['instructions'] || "To merge at least #{@settings['plus_ones_required']} person other than the submitter needs to write a comment with saying _+1_ or :+1:. Then write _!merge_ or :shipit: to trigger the merging."
+        begin
+          @client.add_comment(@project, issue, comment)
+          return 200, "Commented!"
+        rescue Octokit::NotFound
+          return 404, "Octokit returned 404, this could be an issue with your access token"
+        rescue Octokit::Unauthorized
+          return 401, "Authorization to #{@project} failed, please verify your access token"
+        rescue Octokit::TooManyLoginAttempts
+          return 429, "Account for #{@project} has been temporary locked down due to to many failed login attempts"
+        end
+      else
+        return 200, 'Not posting instructions'
+      end
     else
-      return 200, "not enough +1, have #{plus_one.count} but need #{@settings['plus_ones_required']}"
+      return 200, "Unhandled event type #{@event}"
     end
   end
 end
