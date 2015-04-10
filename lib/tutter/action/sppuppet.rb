dc5db45787de2dc546cad3676a972dd66f65c60e
Nik Reiman <nik.reiman@gmail.com>
2015-04-10 09:03:33 +0200

    Clearer documentation wrt :+1: positioning

--- lib/tutter/action/sppuppet.rb
+++ lib/tutter/action/sppuppet.rb
@@ -42,7 +42,7 @@ class Sppuppet
       # If a new pull request is opened, comment with instructions
       if @data['action'] == 'opened' && @settings['post_instructions']
         issue = @data['number']
-        comment = @settings['instructions'] || "To merge at least #{@settings['plus_ones_required']} person other than the submitter needs to write a comment with saying _+1_ or :+1:. Then write _!merge_ or :shipit: to trigger the merging."
+        comment = @settings['instructions'] || "To merge at least #{@settings['plus_ones_required']} person other than the submitter needs to write a comment containing only _+1_ or :+1:. Then write _!merge_ or :shipit: to trigger merging."
         return post_comment(issue, comment)
       else
         return 200, 'Not posting instructions'
